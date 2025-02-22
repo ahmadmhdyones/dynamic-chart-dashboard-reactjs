ab3c6347e84aa8d2c6b2d5d553a55b27f6f913f2
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-02-22 18:18:13 +0400

    Configure Vite with TypeScript and ESLint checker plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,25 @@
+import path from 'path';
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
+import checker from 'vite-plugin-checker';
 
 // ----------------------------------------------------------------------
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    checker({
+      typescript: true,
+      eslint: { lintCommand: 'eslint .' },
+      overlay: {
+        initialIsOpen: false,
+        position: 'tl',
+        // badgeStyle: 'display: none;', // Use this to hide the badge
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
@@ -19,6 +32,7 @@ export default defineConfig({
     port: 8080,
   },
   build: {
+    outDir: 'dist',
     sourcemap: true,
     emptyOutDir: true,
   },
