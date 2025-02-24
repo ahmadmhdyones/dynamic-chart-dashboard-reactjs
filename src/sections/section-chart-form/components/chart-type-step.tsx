167acf7c7c167f6081767fbe2c4ffc2ce30ce6e3
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-02-25 03:56:30 +0400

    refactor: simplify chart type card component and update chart type step

--- src/sections/section-chart-form/components/chart-type-step.tsx
+++ src/sections/section-chart-form/components/chart-type-step.tsx
@@ -1,19 +1,19 @@
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import RadioGroup from '@mui/material/RadioGroup';
 
-import type { ChartType } from '@/types/chart-type.enum';
 import type { ChartTypeOption } from '@/types/chart.types';
 
-import ChartTypeOptionCard from '@/sections/section-chart-form/components/chart-type-option-card';
+import type { ChartType } from '@/models';
+
+import ChartTypeCard from './chart-type-card';
 
 // ----------------------------------------------------------------------
 
-export interface ChartTypeStepProps {
-  options: ChartTypeOption[];
+interface ChartTypeStepProps {
   selectedType: ChartType;
   onTypeChange: (type: ChartType) => void;
+  options: ChartTypeOption[];
 }
 
 function ChartTypeStep({ onTypeChange, options, selectedType }: ChartTypeStepProps) {
@@ -26,20 +26,17 @@ function ChartTypeStep({ onTypeChange, options, selectedType }: ChartTypeStepPro
         Choose the type of chart that best represents your data.
       </Typography>
 
-      <RadioGroup
-        aria-label='chart-type'
-        name='chart-type'
-        onChange={e => onTypeChange(e.target.value as ChartType)}
-        value={selectedType}
-      >
-        <Grid container spacing={2}>
-          {options.map(option => (
-            <Grid item key={option.value} md={4} sm={6} xs={12}>
-              <ChartTypeOptionCard onTypeChange={onTypeChange} option={option} selectedType={selectedType} />
-            </Grid>
-          ))}
-        </Grid>
-      </RadioGroup>
+      <Grid container spacing={3}>
+        {options.map(option => (
+          <Grid item key={option.value} md={4} sm={6} xs={12}>
+            <ChartTypeCard
+              onSelect={() => option.supported && onTypeChange(option.value)}
+              option={option}
+              selected={selectedType === option.value}
+            />
+          </Grid>
+        ))}
+      </Grid>
     </Box>
   );
 }
