6716388a128eb07d8f48b7ab704eebaf5af9f4e1
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-02-24 15:59:42 +0400

    feat: add navigation configuration for dashboard routes

---
+++ src/configs/navigation.config.tsx
@@ -0,0 +1,33 @@
+import type { Navigation } from '@toolpad/core/AppProvider';
+
+import HomeIcon from '@mui/icons-material/Home';
+import SettingsIcon from '@mui/icons-material/Settings';
+import BarChartIcon from '@mui/icons-material/BarChart';
+
+import { paths } from '@/helpers/map-routes';
+
+// ----------------------------------------------------------------------
+
+export const NAVIGATION: Navigation = [
+  { kind: 'header', title: 'Dashboard' },
+  {
+    icon: <HomeIcon />,
+    kind: 'page',
+    segment: paths.dashboard.root.to().slice(1),
+    title: 'Overview',
+  },
+  {
+    icon: <BarChartIcon />,
+    kind: 'page',
+    pattern: `${paths.dashboard.charts.root.to().slice(1)}{/:segment}*`,
+    segment: paths.dashboard.charts.root.to().slice(1),
+    title: 'Charts',
+  },
+  {
+    icon: <SettingsIcon />,
+    kind: 'page',
+    pattern: `${paths.dashboard.settings.to().slice(1)}{/:segment}*`,
+    segment: paths.dashboard.settings.to().slice(1),
+    title: 'Settings',
+  },
+];
