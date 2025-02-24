221a15716ec39b206c6f5d8cbecf42280fd6f64a
Ahmad Yones <ahmadmhdyones@gmail.com>
2025-02-24 17:27:58 +0400

    feat: add react-hot-toast for global notification support

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
+import { Toaster } from 'react-hot-toast';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router';
 import { Provider as StoreProvider } from 'react-redux';
 
-import '@/global.css';
+import './global.css';
 import { store } from '@/stores';
 import { router } from '@/routes';
 import ThemeProvider from '@/theme';
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')!).render(
     <StoreProvider store={store}>
       <ThemeProvider>
         <RouterProvider router={router} />
+        <Toaster position='top-right' reverseOrder={false} />
       </ThemeProvider>
     </StoreProvider>
   </ReactQueryProvider>
